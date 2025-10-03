9ee169de04b64803ebc5182f7497ebfed8dbfa02
Jordan Cook <jordan.cook.git@proton.me>
2025-10-03 18:34:11 -0500

    Add travelmate and wireguard config

--- uci-defaults.sh
+++ uci-defaults.sh
@@ -55,11 +55,79 @@ uci set wireless.radio0.htmode='VHT80'
 uci set wireless.radio0.country='US'
 uci commit wireless
 
+# Configure interface for Travelmate
+uci set network.trm_wwan=interface
+uci set network.trm_wwan.proto='dhcp'
+uci set network.trm_wwan.metric='10'
+
 # Configure DHCP
 uci set dhcp.lan.start='100'
 uci set dhcp.lan.limit='151'
 uci set dhcp.lan.leasetime='12h'
 uci commit dhcp
+
+# Configure firewall
+uci delete firewall.@zone[1].network 2>/dev/null || true
+uci add_list firewall.@zone[1].network='wan'
+uci add_list firewall.@zone[1].network='wwan'
+uci add_list firewall.@zone[1].network='trm_wwan'
+uci commit firewall
+
+# Enable and configure Travelmate
+uci set travelmate.global=travelmate
+uci set travelmate.global.trm_enabled='1'
+uci set travelmate.global.trm_captive='1'
+uci set travelmate.global.trm_netcheck='1'
+uci set travelmate.global.trm_autoadd='0'
+uci set travelmate.global.trm_timeout='60'
+uci set travelmate.global.trm_radio='radio0'
+uci set travelmate.global.trm_iface='trm_wwan'
+uci commit travelmate
+
+# Create WireGuard interface
+uci set network.wg0=interface
+uci set network.wg0.proto='wireguard'
+uci set network.wg0.private_key='REPLACEME'
+uci add_list network.wg0.addresses='REPLACEME'
+uci add_list network.wg0.dns='REPLACEME'
+uci set network.wg0.metric='5'
+
+# Create WireGuard peer
+uci add network wireguard_wg0
+uci set network.@wireguard_wg0[-1].public_key='REPLACEME'
+uci set network.@wireguard_wg0[-1].endpoint_host='REPLACEME'
+uci set network.@wireguard_wg0[-1].endpoint_port='REPLACEME'
+uci set network.@wireguard_wg0[-1].persistent_keepalive='25'
+uci set network.@wireguard_wg0[-1].route_allowed_ips='1'
+uci add_list network.@wireguard_wg0[-1].allowed_ips='0.0.0.0/0'
+uci add_list network.@wireguard_wg0[-1].allowed_ips='::/0'
+
+# Configure WireGuard firewall zone
+uci add firewall zone
+uci set firewall.@zone[-1].name='wgvpn'
+uci set firewall.@zone[-1].input='REJECT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='REJECT'
+uci set firewall.@zone[-1].masq='1'
+uci set firewall.@zone[-1].mtu_fix='1'
+uci add_list firewall.@zone[-1].network='wg0'
+
+# Allow forwarding from LAN to WireGuard
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='lan'
+uci set firewall.@forwarding[-1].dest='wgvpn'
+
+# Allow WireGuard traffic through WAN
+uci add firewall rule
+uci set firewall.@rule[-1].name='Allow-WireGuard'
+uci set firewall.@rule[-1].src='wan'
+uci set firewall.@rule[-1].dest_port='51820'
+uci set firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].target='ACCEPT'
+uci set firewall.@rule[-1].family='ipv4'
+
+uci commit network
+uci commit firewall
 
 # Configure pubkey-only login, if an SSH public key is present
 mkdir -p /etc/dropbear && chmod 700 /etc/dropbear
@@ -72,7 +140,12 @@ if [ -f /etc/dropbear/authorized_keys ]; then
     uci commit dropbear
 fi
 
-service network restart
-service dropbear restart
+# service network restart
+# service dropbear restart
+# service firewall restart
+# service travelmate restart
+
+# Full reboot for PCIe changes to take effect
+( sleep 5; reboot ) &
 
 exit 0
